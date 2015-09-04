228ea41cc13175544955b549cf9a13589594bef0
Matthew Pfister <matthew.pfister@slalo.com>
2015-09-04 18:36:32 -0500

    added in lodash, random response, and moved urls/messages to const doc

--- scripts/main.js
+++ scripts/main.js
@@ -11,43 +11,28 @@
 // Author:
 //   HerrPfister and wickerpopstar
 
+var find = require('lodash/collection/find');
+var isEmpty = require('lodash/lang/isEmpty');
 var consts = require('../consts/gatherer_consts.json');
 
-function findSpecificCard(cardName, cards) {
-  for (var i in cards) {
-    if (cardName.toLowerCase() === cards[i].name.toLowerCase()) {
-      return cards[i];
-    }
-  }
-
-  return undefined;
-}
-
-function getValidCardEdition(card) {
-  for (var i in card.editions) {
-    var editionID = parseInt(card.editions[i].multiverse_id);
-    if (editionID > 0) {
-      return card.editions[i];
-    }
-  }
-
-  return undefined;
-}
+var UrlMap = consts.urlMap;
+var ErrorMsgMap = consts.errorMsgMap;
 
 function getCardDetails(card) {
-  if (!card.editions || card.editions.length === 0) {
+  if (isEmpty(card.editions)) {
     return undefined;
   }
 
-  var cardEdition = getValidCardEdition(card);
-  var multiverseID = cardImage = gathererText = gathererLink = gathererInfo = '';
+  var cardEdition = find(card.editions, function(edition){
+    return parseInt(edition.multiverse_id) > 0;
+  });
+
+  var multiverseID = cardImage = gathererText = '';
 
   if (cardEdition) {
-    multiverseID = cardEdition.multiverse_id;
     cardImage = cardEdition.image_url;
-    gathererText = "View in Gatherer: ";
-    gathererLink = "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=" + multiverseID;
-    gathererInfo = gathererText + gathererLink;
+    multiverseID = cardEdition.multiverse_id;
+    gathererText = "View in Gatherer: " + UrlMap.gatherer + multiverseID;
   }
 
   return {
@@ -57,12 +42,26 @@ function getCardDetails(card) {
     types: card.types,
     cardImage: cardImage,
     subtypes: card.subtypes,
-    gathererInfo: gathererInfo,
+    gathererText: gathererText,
     attributes: (card.power) ? card.power + "/" + card.toughness : ''
   };
 }
 
 module.exports = function (robot) {
+  robot.respond(/gatherer random/i, function(res){
+    // TODO: Find out what the seed number should be
+    var multiverseid = Math.floor(Math.random() * 50000);
+
+    robot.http(UrlMap.multiverseid + multiverseid)
+      .header('Accept', 'application/json')
+      .get()(function(error, response, body) {
+        if (error) {
+          res.send(ErrorMsgMap.default)
+        } else {
+          // TODO: return random card info
+        }
+      });
+  });
 
   // As of right now, we are just capturing everything after the gatherer command.
   // However, later we can make it more customizable if we want.
@@ -72,63 +71,67 @@ module.exports = function (robot) {
     // Grab the captured user's input
     var cardName = res.match[1];
 
-    robot.http('https://api.deckbrew.com/mtg/cards?name=' + cardName)
+    // This endpoint returns a list of 10 cards (the same format as the /mtg/cards endpoint)
+    // that match the beginning of the search term. This endpoint is great for building interactive
+    // autocomplete search for Magic cards.
+    robot.http(UrlMap.cardName + cardName)
       .header('Accept', 'application/json')
       .get()(function(error, response, body) {
-          if (error) {
-            res.send('There was an issue with your request. Please try again later.');
-          } else {
-            var cards = JSON.parse(body);
-            var cardCount = cards.length;
+        if (error) {
+          res.send(ErrorMsgMap.default);
+        } else {
+          var cards = JSON.parse(body);
 
-            // If there are no cards returned then that means the card either doesn't exist
-            // or the user misspelled its' name. We need to notify them.
-            if (cardCount === 0) {
-              res.send('We could not find the card ' + cardName + '. Please try again.');
+          // If there are no cards returned then that means the card either doesn't exist
+          // or the user misspelled its' name. We need to notify them.
+          if (isEmpty(cards)) {
+            res.send('We could not find the card ' + cardName + '. Please try again.');
+            return;
+          }
+
+          var card = find(cards, function(card){
+            return cardName.toLowerCase() === card.name.toLowerCase()
+          });
+
+          // If findSpecificCard comes back with a match that means, we hope,
+          // it found the exact card the user was looking for. Otherwise, that means
+          // the service has found more than one match.
+          if (card) {
+            var cardDetails = getCardDetails(card);
+
+            // If getCardDetails returns undefined then something strange
+            // is going on with the data returned by the service. Otherwise,
+            // display all the relevant data.
+            if (!cardDetails) {
+              res.send(ErrorMsgMap.cardDetail);
               return;
             }
 
-            var card = findSpecificCard(cardName, cards);
-
-            // If findSpecificCard comes back with a match that means, we hope,
-            // it found the exact card the user was looking for. Otherwise, that means
-            // the service has found more than one match.
-            if (card) {
-              var cardDetails = getCardDetails(card);
-
-              // If getCardDetails returns undefined then something strange
-              // is going on with the data returned by the service. Otherwise,
-              // display all the relevant data.
-              if (!cardDetails) {
-                res.send('There was an issue with retrieving the details on the card you were looking for. Please try again.');
-                return;
-              }
-
-              if (cardDetails.cardImage){
-                res.send(cardDetails.cardImage);
-              } else {
-                res.send(cardDetails.name);
-                res.send(cardDetails.text);
-                res.send(cardDetails.cost);
-                res.send(cardDetails.types);
-                res.send(cardDetails.subtypes);
-                res.send(cardDetails.attributes);
-              }
-
-              res.send(cardDetails.gathererInfo);
-
+            if (cardDetails.cardImage){
+              res.send(cardDetails.cardImage);
             } else {
-              // Grab the first X amount of cards, which is determined from the constant cardLimit.
-              // Then print off the name of each card.
-              var cardLimit = consts.cardLimit;
-              var cardSample = cards.slice(0, cardLimit);
+              res.send(cardDetails.name);
+              res.send(cardDetails.text);
+              res.send(cardDetails.cost);
+              res.send(cardDetails.types);
+              res.send(cardDetails.subtypes);
+              res.send(cardDetails.attributes);
+            }
 
-              res.send('Displaying ' + cardSample.length + ' out of ' + cardCount + ' cards:');
-              for (var i in cardSample) {
-                res.send(cardSample[i].name);
-              }
+            res.send(cardDetails.gathererText);
+
+          } else {
+            // Grab the first X amount of cards, which is determined from the constant cardLimit.
+            // Then print off the name of each card.
+            var cardLimit = consts.cardLimit;
+            var cardSample = cards.slice(0, cardLimit);
+
+            res.send('Displaying ' + cardSample.length + ' out of ' + cards.length + ' cards:');
+            for (var i in cardSample) {
+              res.send(cardSample[i].name);
             }
           }
-        });
+        }
+      });
   });
 };
