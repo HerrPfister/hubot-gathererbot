048b298ed02ac155f52a04d383fc2e30e4097fb2
wickerpopstar <patrick.b.powers@gmail.com>
2015-09-04 01:51:08 -0500

    Revert "fleshed out functionality"
    This reverts commit 1d5a8cf23ed55fda6329284b45618b8186470da1.

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,7 @@
 //   A simple bot that queries the deckbrew service for specific cards.
 //
 // Commands:
-//   hubot gatherer [card name] - queries the card service and returns exact match
-//                                if it exists, or names of full search results.
+//   hubot gatherer [card name] - queries the card service and returns the first card the query found
 //
 // Notes:
 //   <optional notes required for the script>
@@ -27,71 +26,18 @@ module.exports = function (robot) {
           if (error) {
             res.send('There was an issue with your request. Please try again later.');
           } else {
-            var displayCount = 5;
-            var cards = JSON.parse(body);
+            // For simplicity's sake I am just grabbing the first card from the query,
+            // and then using the image from the first edition in that card's "editions" list.
 
-            // Count the number of exactly matching results. If there is one exact
-            // match, but multiple matches, should only print exact match, not list
-            var cardCount = 0;
-            for (var i = 0; i < cards.length; i++) {
-              if(cards[i].name.toLowerCase() === cardName.toLowerCase()){
-                cardCount++;
-              }
-            }
+            var card = JSON.parse(body)[0];
 
-            // If there are multiple matches from a search, and no exact match,
-            // print a list of the names of the first 5 cards (or fewer if
-            // there are fewer than five results) in alphabetical order
-            if ((cardCount !== 1) && (cards.length > 1)) {
-              var numOut = displayCount < cards.length ? displayCount : cards.length;
-              res.send('Displaying top ' + numOut + ' of ' + cards.length + ' : \n');
+            if (card) {
+              var cardImage = card.editions[0].image_url;
 
-              // ToDo: Instead of just text, this can be a gatherer link.
-              for (var j = 0; j < numOut; j++) {
-                res.send(cards[j].name);
-              }
-
-              var remaining = cards.length - numOut;
-              if (remaining > 0)
-                res.send("...\n" + remaining + ' other results matching search: "' + cardName + '"');
-            }
-            else
-            {
-              var card = cards[0];
-
-              if (card) {
-                // get multiverse_id that isn't 0 (non-set editions don't have a multiverse_id)
-                var multiverse_id = '0';
-                var cardImage;
-
-                for (var k = 0; k < card.editions.length; k++) {
-                  if (card.editions[k].multiverse_id !== '0') {
-                    multiverse_id = card.editions[k].multiverse_id;
-                    cardImage = card.editions[k].image_url;
-                  }
-                }
-
-                // If the object has an image print that. Otherwise, print the rules data.
-                if (true){
-                  res.send(cardImage);
-                }
-                else {
-                  res.send(card.name);
-                  res.send(card.cost);
-                  res.send(card.types);
-                  res.send(card.subtypes);
-                  res.send(card.text);
-                  if (card.power)
-                    res.send(card.power + "/" + card.toughness);
-                }
-
-                // add link to view in Gatherer
-                var gathererText = "View in Gatherer";
-                var gathererLink = gathererText.link('http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' + multiverse_id);
-                res.send(gathererLink);
-              } else {
-                res.send('We could not find the card ' + cardName + '. Please try again.');
-              }
+              // If the object has an image send that back. Otherwise, send back the raw JSON.
+              res.send(cardImage || card);
+            } else {
+              res.send('We could not find the card ' + cardName + '. Please try again.');
             }
           }
         });
