15e388c3206d91826a426571be3d0ab72f09d12c
Matthew Pfister <matthew.pfister@slalo.com>
2015-09-05 09:17:30 -0500

    finished random command, and refactoring

---
+++ consts/consts.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "cardLimit": 5,
+  "verseIDSeed": 4980,
+  "urlMap": {
+    "gatherer": "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=",
+    "cardName": "https://api.deckbrew.com/mtg/cards?name=",
+    "multiverseid": "https://api.deckbrew.com/mtg/cards?multiverseid="
+  },
+  "errorMessageMap" : {
+    "default": "There was an issue with your request. Please try again.",
+    "cardDetail": "There was an issue with retrieving the details on the card you were looking for. Please try again.",
+    cardNotFound: function(cardName) { return "We could not find the card " + cardName + ". Please try again."; },
+    verseIdNotFound: function(multiverseid) { return "We could not find the card with multiverse id" + multiverseid + ". Please try again."; }
+  },
+  "messageMap": {
+    cardPoolSize: function (sampleSize, poolSize) { return 'Displaying ' + sampleSize + ' out of ' + poolSize + ' cards:'; }
+  }
+};
--- scripts/main.js
+++ scripts/main.js
@@ -13,10 +13,11 @@
 
 var find = require('lodash/collection/find');
 var isEmpty = require('lodash/lang/isEmpty');
-var consts = require('../consts/gatherer_consts.json');
+var consts = require('../consts/consts');
 
 var UrlMap = consts.urlMap;
-var ErrorMsgMap = consts.errorMsgMap;
+var MessageMap = consts.messageMap;
+var ErrorMessageMap = consts.errorMessageMap;
 
 function getCardDetails(card) {
   if (isEmpty(card.editions)) {
@@ -27,12 +28,12 @@ function getCardDetails(card) {
     return parseInt(edition.multiverse_id) > 0;
   });
 
-  var multiverseID = cardImage = gathererText = '';
+  var multiverseid = cardImage = gathererText = '';
 
   if (cardEdition) {
     cardImage = cardEdition.image_url;
-    multiverseID = cardEdition.multiverse_id;
-    gathererText = "View in Gatherer: " + UrlMap.gatherer + multiverseID;
+    multiverseid = cardEdition.multiverse_id;
+    gathererText = "View in Gatherer: " + UrlMap.gatherer + multiverseid;
   }
 
   return {
@@ -47,90 +48,95 @@ function getCardDetails(card) {
   };
 }
 
+function sendDetails(res, cardDetails) {
+  // If cardDetails is undefined then something strange
+  // is going on with the data returned by the service. Otherwise,
+  // display all the relevant data.
+  if (!cardDetails) {
+    res.send(ErrorMessageMap.cardDetail);
+    return;
+  }
+
+  if (cardDetails.cardImage){
+    res.send(cardDetails.cardImage);
+  } else {
+    res.send(cardDetails.name);
+    res.send(cardDetails.text);
+    res.send(cardDetails.cost);
+    res.send(cardDetails.types);
+    res.send(cardDetails.subtypes);
+    res.send(cardDetails.attributes);
+  }
+
+  res.send(cardDetails.gathererText);
+}
+
 module.exports = function (robot) {
   robot.respond(/gatherer random/i, function(res){
-    // TODO: Find out what the seed number should be
+    // NOTE: We are using a seed that may change over time.
+    // We need a more dynamic way of getting the max multiverseid.
+    // This will generate an id between 1 and 4980.
     var multiverseid = Math.floor(Math.random() * (consts.verseIDSeed - 1) + 1);
 
     robot.http(UrlMap.multiverseid + multiverseid)
       .header('Accept', 'application/json')
       .get()(function(error, response, body) {
         if (error) {
-          res.send(ErrorMsgMap.default)
+          res.send(ErrorMessageMap.default)
         } else {
-          //TODO: Parse Body
-          console.log(body);
+          var card = JSON.parse(body);
+
+          if (!isEmpty(card)) {
+            var cardDetails = getCardDetails(card[0]);
+            sendDetails(res, cardDetails);
+          } else {
+            res.send(ErrorMessageMap.multiverseIdNotFound(multiverseid));
+          }
         }
       });
   });
 
-  // As of right now, we are just capturing everything after the gatherer command.
-  // However, later we can make it more customizable if we want.
+  // NOTE: As of right now, we are just capturing everything after the find
+  // command. However, later we can make it more customizable if we want.
   // i.e. getting subtypes, color, rarity, etc.
   robot.respond(/gatherer\sfind\s(.*)/i, function (res) {
 
     // Grab the captured user's input
     var cardName = res.match[1];
 
-    // This endpoint returns a list of 10 cards (the same format as the /mtg/cards endpoint)
-    // that match the beginning of the search term. This endpoint is great for building interactive
-    // autocomplete search for Magic cards.
     robot.http(UrlMap.cardName + cardName)
       .header('Accept', 'application/json')
       .get()(function(error, response, body) {
         if (error) {
-          res.send(ErrorMsgMap.default);
+          res.send(ErrorMessageMap.default);
         } else {
           var cards = JSON.parse(body);
 
-          // If there are no cards returned then that means the card either doesn't exist
-          // or the user misspelled its' name. We need to notify them.
-          if (isEmpty(cards)) {
-            res.send('We could not find the card ' + cardName + '. Please try again.');
-            return;
-          }
+          if (!isEmpty(cards)) {
+            var card = find(cards, function(card){
+              return cardName.toLowerCase() === card.name.toLowerCase()
+            });
 
-          var card = find(cards, function(card){
-            return cardName.toLowerCase() === card.name.toLowerCase()
-          });
+            // If find() comes back with a match that means it found the exact card the user was
+            // looking for. Otherwise, that means the service has found more than one match.
+            if (card) {
+              var cardDetails = getCardDetails(card);
+              sendDetails(res, cardDetails);
 
-          // If findSpecificCard comes back with a match that means, we hope,
-          // it found the exact card the user was looking for. Otherwise, that means
-          // the service has found more than one match.
-          if (card) {
-            var cardDetails = getCardDetails(card);
-
-            // If getCardDetails returns undefined then something strange
-            // is going on with the data returned by the service. Otherwise,
-            // display all the relevant data.
-            if (!cardDetails) {
-              res.send(ErrorMsgMap.cardDetail);
-              return;
-            }
-
-            if (cardDetails.cardImage){
-              res.send(cardDetails.cardImage);
             } else {
-              res.send(cardDetails.name);
-              res.send(cardDetails.text);
-              res.send(cardDetails.cost);
-              res.send(cardDetails.types);
-              res.send(cardDetails.subtypes);
-              res.send(cardDetails.attributes);
+              // Grab the first X amount of cards, which is determined from the constant cardLimit.
+              // Then print off the name of each card.
+              var cardLimit = consts.cardLimit;
+              var cardSample = cards.slice(0, cardLimit);
+
+              res.send(MessageMap.cardPoolSize(cardSample.length, cards.length));
+
+              for (var i in cardSample) {
+                res.send(cardSample[i].name);
+              }
             }
-
-            res.send(cardDetails.gathererText);
-
           } else {
-            // Grab the first X amount of cards, which is determined from the constant cardLimit.
-            // Then print off the name of each card.
-            var cardLimit = consts.cardLimit;
-            var cardSample = cards.slice(0, cardLimit);
-
-            res.send('Displaying ' + cardSample.length + ' out of ' + cards.length + ' cards:');
-            for (var i in cardSample) {
-              res.send(cardSample[i].name);
-            }
+            res.send(ErrorMessageMap.cardNotFound(cardName));
           }
         }
       });
