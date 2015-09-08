ca123143f6b74a7ffe451d2aa74f0df11be4ce4e
Matthew Pfister <matthew.pfister@slalo.com>
2015-09-08 10:33:55 -0500

    updated version number and message map

--- static/consts.js
+++ static/consts.js
@@ -9,15 +9,15 @@ module.exports = {
   "errorMessageMap" : {
     "default": "There was an issue with your request. Please try again.",
     "findCommandError": "Invalid parameters. Please make sure that the card name is included in the parameters.",
-    "cardDetail": "There was an issue with retrieving the details on the card you were looking for. Please try again.",
+    "cardDetail": "There was an issue with retrieving the details of the card you were looking for. Please try again.",
     cardNotFound: function(cardName) { return "We could not find the card " + cardName + ". Please try again."; },
     verseIdNotFound: function(multiverseid) { return "We could not find the card with multiverse id" + multiverseid + ". Please try again."; }
   },
   "messageMap": {
-    "clashDraw": "It's a draw! Try again if you still can't settle the debate.",
+    "clashDraw": "It's a draw! Try again if you still can't settle the dispute.",
     clashWinner: function (winnerName) { return 'Clash resolved! ' + winnerName + ' is the winner!'; },
-    clashCardDraw: function (name, card) { return name + ' drew ' + card.name + ' which has a converted mana cost of ' + card.cmc; },
-    clashDefault: function (challenger, opponent) { return challenger + ' challenges ' + opponent + ' to a mtg clash.'; },
-    cardPoolSize: function (sampleSize, poolSize) { return 'Displaying ' + sampleSize + ' out of ' + poolSize + ' cards:'; }
+    clashCardDraw: function (name, card) { return name + ' drew ' + card.name + ', which has a converted mana cost of ' + card.cmc + '.'; },
+    clashDefault: function (challenger, opponent) { return challenger + ' challenges ' + opponent + ' to a mtg clash!'; },
+    cardPoolSize: function (sampleSize, poolSize) { return 'Displaying ' + sampleSize + ' out of ' + poolSize + ' matched cards:'; }
   }
 };
