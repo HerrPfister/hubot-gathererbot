f8f56df7700e021e9cb2f2783d74776791adff39
Matthew Pfister <matthew.pfister@slalo.com>
2015-09-23 23:38:12 -0500

    added in advance gatherer link when a list is printed out

--- static/consts.js
+++ static/consts.js
@@ -1,26 +1,26 @@
 module.exports = {
     defaultError: 'There was an issue with your request. Please try again.',
     gathererColorMap: {
-        blue: "u",
-        red: "r",
-        green: "g",
-        black: "b",
-        white: "w"
+        blue: 'u',
+        red: 'r',
+        green: 'g',
+        black: 'b',
+        white: 'w'
     },
     gathererRarityMap: {
-        common: "c",
-        uncommon: "u",
-        rare: "r",
-        mythic: "m"
+        common: 'c',
+        uncommon: 'u',
+        rare: 'r',
+        mythic: 'm'
     },
     gathererUrlKeyMap: {
-        color: "color=+",
-        format: "format=+",
-        oracle: "text=+",
-        rarity: "rarity=+",
-        set: "set=+",
-        subtype: "subtype=+",
-        type: "type=+"
+        color: 'color=+',
+        format: 'format=+',
+        oracle: 'text=+',
+        rarity: 'rarity=+',
+        set: 'set=+',
+        subtype: 'subtype=+',
+        type: 'type=+'
     },
     responseErrorCodes: [
         400,
@@ -30,7 +30,7 @@ module.exports = {
         deckBrewBase: 'https://api.deckbrew.com/mtg/cards?',
         deckBrewMultiverseId: 'https://api.deckbrew.com/mtg/cards?multiverseid=',
         gatherer: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=',
-        gathererAdvanced: "http://gatherer.wizards.com/Pages/Search/Default.aspx?action=advanced&",
+        gathererAdvanced: 'http://gatherer.wizards.com/Pages/Search/Default.aspx?action=advanced&',
         gathererRandom: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random'
     }
 };
