0dce5efac0980b1777fb4d8a89305103fc415d87
Matthew Pfister <herrpfister@gmail.com>
2017-04-04 15:57:37 -0500

    Cleaning up main comment in main.js

--- src/main.js
+++ src/main.js
@@ -3,21 +3,11 @@
 //
 // Commands:
 //   hubot mtg query [search param1]=[search value1],[search value2]|[search value3] & [search param2]= ...
-//         - This command will return up to 5 cards with the given params.
-//           NOTE: separate value lists with a "|" to make an OR clause
-//                 and a "," for an AND clause. Separate params with a "&"
-//
 //   hubot mtg find [card name]
-//         - Returns the card with a name that exactly matches the passed in name, if it exists.
-//
 //   hubot mtg random
-//         - This command return a random card.
-//
 //   hubot mtg clash [@handler]
-//         - This command will get two random cards and then compare their converted mana costs to see which is higher.
-//
 //   hubot mtg term [term]
-//         - This command will get the definition of a magic term. For instance, trample.
+//
 // Author:
 //   HerrPfister
 
